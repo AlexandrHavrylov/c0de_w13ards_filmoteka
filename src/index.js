52c287c88607063cce20ba1a49c3c21f17e4f345
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-08-07 15:17:03 +0300

    added trending and search items fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+// код для проверки работоспособности запросов
+
+// import './js/fetch-items';
+
+// import ItemsApiService from './js/fetch-items.js';
+
+// const itemsApiService = new ItemsApiService();
+
+// console.log(itemsApiService);
+
+// function onLoad() {
+//   console.log(itemsApiService.fetchTrandingItems());
+
+// Log ниже будет работать если подставить значение в search querry
+//   console.log(itemsApiService.fetchItemsFromSearch());
+// }
+
+// onLoad();
