474637d6de9bffd6e6c02710fa3a406a61a537b3
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-08-08 10:44:00 +0300

    Create header-switch.js

---
+++ src/js/header-switch.js
@@ -0,0 +1,25 @@
+/*
+
+
+*/
+
+const HEADER_ENUM = {
+  HOME: 'home',
+  LIBRARY: 'library',
+};
+
+class HeaderSwitcher {
+  constructor(
+    options = {
+      currentHeader: HEADER_ENUM.HOME,
+      hideClassCss: 'is-hidden',
+      bgImages: { home: '' },
+    },
+  ) {
+    // this.currentHeader = options.currentHeader;
+    // this.hideClassCss = options.hideClassCss;
+    //   this.refs = {
+    //       bntHome =
+    // };
+  }
+}
