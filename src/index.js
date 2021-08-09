ed01a864da2933a1ea5657a7c55e2e17a25516e9
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-08-09 22:35:57 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,69 +1,17 @@
-import { getRefs } from './js/get-refs';
 import './sass/main.scss';
-import ItemsApiService from './js/fetch-items.js';
-import galleryMarkup from './templates/filmsInGallery.hbs';
+import { getRefs } from './js/get-refs';
 import { onSearchFormInput } from './js/fetch-by-name';
-
-import { onSerchBtnClick } from './js/fetch-by-name';
-import { onHomeBtnClick } from './js/on-home-btn';
-import genresTranslator from './genres';
-=======
 import { onSearchBtnClick } from './js/fetch-by-name';
-import { GetPopularFilms } from './js/get-popular-films';
 import { HeaderSwitcher, HEADER_ENUM } from './js/header-switch';
+import { GetPopularFilms } from './js/get-popular-films';
 
-
-const itemsApiService = new ItemsApiService();
 
 const refs = getRefs();
-
-=======
 
 refs.searchForm.addEventListener('input', onSearchFormInput);
 refs.searchBtn.addEventListener('click', onSearchBtnClick);
 
 
-
-
-const fetchMovies = async () => {
-  const result = await itemsApiService.fetchTrandingItems();
-
-  const updatedMovies = result.map(movie => {
-    const genres = movie.genre_ids.map(genreId => {
-      const currentGenre = genresTranslator.find(singleGenre => singleGenre.id === genreId);
-
-      return ' ' + currentGenre.name;
-    });
-
-    const movieYear = movie.release_date.slice(0, 4);
-
-    if (genres.length > 3) {
-      const cutGenres = genres.slice(0, 2);
-
-      const modifiedGenre = [...cutGenres, ' Other'];
-
-      return {
-        ...movie,
-        genres: modifiedGenre,
-        movieYear,
-      };
-    }
-
-    return {
-      ...movie,
-      genres,
-      movieYear,
-    };
-  });
-
-  return updatedMovies;
-};
-fetchMovies().then(movies => (refs.moviesList.innerHTML = galleryMarkup(movies)));
-
-refs.searchForm.addEventListener('input', onSearchFormInput);
-refs.searchBtn.addEventListener('click', onSerchBtnClick);
-refs.homeBtn.addEventListener('click', onHomeBtnClick);
-=======
 const headerSwitcher = new HeaderSwitcher({
   onChangeCallback: page => {
     switch (page) {
@@ -79,4 +27,6 @@ const headerSwitcher = new HeaderSwitcher({
 });
 
 
-function GetLibraryFilms() {}
+function GetLibraryFilms() {
+
+}
--- src/js/get-popular-films.js
+++ src/js/get-popular-films.js
@@ -2,16 +2,46 @@
 import ItemsApiService from "./fetch-items";
 import galleryMarkup from '../templates/filmsInGallery.hbs'
 import { getRefs } from "./get-refs";
-
+import genresTranslator from '../genres';
 
 
 const itemsApiService = new ItemsApiService();
 const refs = getRefs()
 function GetPopularFilms() {
-  //фетчим популярные фильмы
+const fetchMovies = async () => {
+  const result = await itemsApiService.fetchTrandingItems();
 
-  const fetchMovies = () => itemsApiService.fetchTrandingItems();
-  fetchMovies().then(result => (refs.moviesList.innerHTML = galleryMarkup(result)));
+  const updatedMovies = result.map(movie => {
+    const genres = movie.genre_ids.map(genreId => {
+      const currentGenre = genresTranslator.find(singleGenre => singleGenre.id === genreId);
+
+      return ' ' + currentGenre.name;
+    });
+
+    const movieYear = movie.release_date.slice(0, 4);
+
+    if (genres.length > 3) {
+      const cutGenres = genres.slice(0, 2);
+
+      const modifiedGenre = [...cutGenres, ' Other'];
+
+      return {
+        ...movie,
+        genres: modifiedGenre,
+        movieYear,
+      };
+    }
+
+    return {
+      ...movie,
+      genres,
+      movieYear,
+    };
+  });
+
+  return updatedMovies;
+};
+fetchMovies().then(movies => (refs.moviesList.innerHTML = galleryMarkup(movies)));
 }
 
 export {GetPopularFilms}
