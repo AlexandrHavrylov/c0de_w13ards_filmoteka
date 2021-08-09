e2d81430bb2f61820d265a1586f68731404662a1
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-08-09 21:53:34 +0300

    Update userLibrary.js

--- src/js/userLibrary.js
+++ src/js/userLibrary.js
@@ -1,9 +1,10 @@
 const defaultOptions = {
-  isSelectedStyle: 'selectedStyle',
+  isSelectedStyle: 'form__btn--current',
   buttons: {
-    watched: 'watched',
-    queue: 'queue',
+    watched: '#watched',
+    queue: '#queue',
   },
+  cardContainer: '',
 };
 
 const USER_LIBRARY_ENUM = {
@@ -13,20 +14,36 @@ const USER_LIBRARY_ENUM = {
 this.#curLibrary = USER_LIBRARY_ENUM.WATCHED;
 this.#refs = {};
 
+const CLASSLIST_ACTION = {
+  ADD: 'add',
+  REMOVE: 'remove',
+};
+
 class UserLibrary {
   constructor(args) {
-    const options = { ...defaultOptions, ...args };
+    this.options = { ...defaultOptions, ...args };
 
     this.#refs.btnWatched = document
       .querySelector(options.buttons.watched)
-      .addEventListener('click', e => {});
+      .addEventListener('click', e => {
+        this.#setSelectedStyle(CLASSLIST_ACTION.ADD, CLASSLIST_ACTION.REMOVE);
+        this.#curLibrary = USER_LIBRARY_ENUM.WATCHED;
+      });
     this.#refs.btnQueue = document
       .querySelector(options.buttons.queue)
-      .addEventListener('click', e => {});
+      .addEventListener('click', e => {
+        this.#setSelectedStyle(CLASSLIST_ACTION.REMOVE, CLASSLIST_ACTION.ADD);
+        this.#curLibrary = USER_LIBRARY_ENUM.QUEUE;
+      });
   }
 
-  add(item) {}
+  #setSelectedStyle(action1, action2) {
+    this.#refs.btnQueue.classList[action1](this.options.isSelectedStyle);
+    this.#refs.btnWatched.classList[action2](this.options.isSelectedStyle);
+  }
+
+  add(card) {}
   all() {}
-  update(item) {}
-  remove(item) {}
+  update(card) {}
+  remove(card) {}
 }
