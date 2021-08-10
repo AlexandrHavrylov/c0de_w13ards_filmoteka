6d3f6258b8bfbdeb0a1ec465efe0a98e8f8a7719
Yana Sokurenko <yanamoss@gmail.com>
2021-08-10 12:41:33 +0300

    Update getCardMovie.js
    Добавление закрытия по Esc.

--- src/js/getCardMovie.js
+++ src/js/getCardMovie.js
@@ -3,10 +3,12 @@ import { getRefs } from './get-refs';
 import ItemsApiService from './fetch-items.js';
 import filmInModal from '../templates/filmInModal.hbs';
 
+
 const itemsApiService = new ItemsApiService();
 const refs = getRefs();
 let modalMovieClose;
 
+// Открытие модального окна с готовой карточкой
 function openCardMovie(event) {
    const movieId = event.target.parentNode.dataset.id;
    if (movieId) {
@@ -15,16 +17,36 @@ function openCardMovie(event) {
    
 };
 
+// Отправка запроса и формирование карточки
 async function renderCard(movieId) {
    await itemsApiService.fetchCard(movieId)
       .then((response) => {
          refs.modalMovie.innerHTML = filmInModal(response);
          refs.modalMovie.classList.remove(('visually-hidden'))
          modalMovieClose = document.querySelector('.modal-movie__button--close');
-         modalMovieClose.addEventListener('click', closeCardMovie);
+         modalMovieClose.addEventListener('click', closeCard);
+         modalMovieClose.addEventListener('keydown', closeCardEsc);
       })
+   // добавление слушателей после формирования карточки
+         modalMovieClose.addEventListener('click', closeCard);
+         window.addEventListener('keydown', closeCardEsc);
 };
 
-const closeCardMovie = () => refs.modalMovie.classList.add('visually-hidden');
+// Закрытие модального окна по событию
+const closeCard = () => closeCardMovie();
+const closeCardEsc = event => {
+   if (event.key === "Escape") {
+      closeCardMovie();
+   };
+};
+
+// Функция закрытия
+const closeCardMovie = () => {
+   refs.modalMovie.classList.add('visually-hidden');
+   
+   // Удаление слушателей
+   modalMovieClose.removeEventListener('click', closeCard);
+   window.removeEventListener('keydown', closeCardEsc);
+};
 
 export { openCardMovie, renderCard };
