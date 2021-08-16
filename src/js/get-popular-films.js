fe2d964501d6e2aebee538439b88b05545ab4ef2
chillhaze <alexander.gotra@gmail.com>
2021-08-17 00:13:14 +0300

    pagination mobile version added (pages count)

--- src/js/get-popular-films.js
+++ src/js/get-popular-films.js
@@ -26,23 +26,47 @@ async function getPopularFilms() {
   // ========== Создание пагинации ==========
   // Общее количество полученных страниц храним в numberOfPages
   numberOfPages = result.total_pages;
+  // console.log(numberOfPages);
 
- 
   // Пагинация
   const container = document.getElementById('pagination');
-  const pagination = new Pagination(container, options, (options.totalItems = numberOfPages));
 
-  // События при навигации
-  pagination.on('afterMove', event => {
-    const currentPage = event.page;
-    // console.log(currentPage);
+  // Читаем ширину экрана
+  let deviceWidth = document.documentElement.clientWidth;
+  // Количество видимых страниц пагинации
+  let pagesToShow = 5;
 
-    // Загрузка и отрисовка выбранной страницы
-    renderTrandingPage(currentPage);
-  });
+  // Если ширина экрана менее 520px отображаем 3 страницы пагинации
+  if (deviceWidth < 520) {
+    pagesToShow = 3;
 
+    const pagination = new Pagination(
+      container,
+      options,
+      ((options.totalItems = numberOfPages), (options.visiblePages = pagesToShow)),
+    );
 
+    // События при навигации
+    pagination.on('afterMove', event => {
+      const currentPage = event.page;
+      // console.log(currentPage);
 
+      // Загрузка и отрисовка выбранной страницы
+      renderTrandingPage(currentPage);
+    });
+  } else {
+    // Если ширина экрана менее 520px отображаем 5 страницы пагинации (по умолчанию 5)
+    const pagination = new Pagination(container, options, (options.totalItems = numberOfPages));
+
+    // События при навигации
+    pagination.on('afterMove', event => {
+      const currentPage = event.page;
+      // console.log(currentPage);
+
+      // Загрузка и отрисовка выбранной страницы
+      renderTrandingPage(currentPage);
+    });
+  }
 }
 
 export { getPopularFilms };
